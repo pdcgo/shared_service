5b1fbd08099456f1f8c7619deeedfd735741fe73
vaziria <manorder123@gmail.com>
2025-10-31 15:54:24 +0700

    adding hello service

--- services/hello_service/service.go
+++ services/hello_service/service.go
@@ -5,13 +5,20 @@ import (
 
 	"connectrpc.com/connect"
 	"github.com/pdcgo/schema/services/access_iface/v1"
+	"github.com/pdcgo/shared/custom_connect"
 )
 
 type helloServiceImpl struct{}
 
 // Hello implements access_ifaceconnect.HelloServiceHandler.
-func (h *helloServiceImpl) Hello(context.Context, *connect.Request[access_iface.HelloRequest]) (*connect.Response[access_iface.HelloResponse], error) {
-	panic("unimplemented")
+func (h *helloServiceImpl) Hello(
+	ctx context.Context,
+	req *connect.Request[access_iface.HelloRequest]) (*connect.Response[access_iface.HelloResponse], error) {
+	return &connect.Response[access_iface.HelloResponse]{
+		Msg: &access_iface.HelloResponse{
+			Source: custom_connect.GetRequestSource(ctx),
+		},
+	}, nil
 }
 
 func NewHelloService() *helloServiceImpl {
